252d5e41f586d4dfd8fc602fd2aed4fc44bb7a11
Alex Salley <lexi27sal@gmail.com>
2026-09-18 09:08:32 -0400

    this close to digging done

---
+++ Game Project/Capstone_AshbornStudios/Assets/Scripts/DigVisuals/meshController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class meshController : MonoBehaviour
+{
+    
+    public Mesh mesh;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    // Update is called once per frame
+    
+}
